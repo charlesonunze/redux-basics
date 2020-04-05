fe18e948244fc2c0ce10286316eed1de71c5ef3b
charlesonunze <charlesonunze2@gmail.com>
2020-04-05 12:26:24 +0100

    dispatch actions

--- redux-basic.js
+++ redux-basic.js
@@ -5,14 +5,32 @@ const initialState = { counter: 0 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+	if (action.type === 'INCREMENT_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + 1,
+		};
+	}
+
+	if (action.type === 'ADD_TO_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + action.value,
+		};
+	}
+
 	return state;
 };
 
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
-console.log(store);
 
 // Action
+store.dispatch({ type: 'INCREMENT_COUNTER' });
+console.log(store.getState());
+
+store.dispatch({ type: 'ADD_TO_COUNTER', value: 5 });
+console.log(store.getState());
 
 // Subscription
